720c01f4101ce22b09836e5da110e23e2a87343a
lrkhan <lrkhan@umich.edu>
2021-11-08 10:12:40 -0500

    lrkhan code

---
+++ Hype/HypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypeApp.swift
+//  Hype
+//
+//  Created by Luthfor Khan on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
